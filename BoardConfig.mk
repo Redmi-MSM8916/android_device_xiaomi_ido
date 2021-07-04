b019fd2b85811f43f3e2602dcf138e44b3e41780
xoo2001 <ekora2001@gmail.com>
2021-07-04 15:08:57 +0700

    ido: fix kernel target build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,7 +72,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_ido
 
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8916
+TARGET_KERNEL_SOURCE := kernel/xiaomi/ido
 TARGET_KERNEL_CONFIG := ido_defconfig
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
