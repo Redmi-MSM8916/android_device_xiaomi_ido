44e6a69429bc70b0d39a62eee499d507dad2e03c
xoo2001 <ekora2001@gmail.com>
2021-08-10 07:03:07 +0700

    ido: switch to redfin fp and use SecPath august

--- komodo_ido.mk
+++ komodo_ido.mk
@@ -37,8 +37,7 @@ PRODUCT_DEVICE := ido
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Build fingerprint
-BUILD_FINGERPRINT="Xiaomi/ido/ido:5.1.1/LMY47V/V9.6.1.0.LAICNFD:user/release-keys"
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="ido-user 5.1.1 LMY47V V9.6.1.0.LAICNFD release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys"
+
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
