7ca6e3006de11049daa096e273ebd5ddb457718a
xoo2001 <ekora2001@gmail.com>
2021-12-07 11:29:28 +0000

    ido: Set name BT
    Signed-off-by: xoo2001 <ekora2001@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME        "Lenovo A6020"
+#define BTM_DEF_LOCAL_NAME        "Redmi 3"
 
 #define MAX_ACL_CONNECTIONS       7
 #define BLUETOOTH_QTI_SW          TRUE
